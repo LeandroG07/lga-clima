70701627c55548cc11f77b5bc98012d57bec5637
Leandro Galli <legalli1993@gmail.com>
2022-05-10 19:42:43 -0300

    config inicial controller previsao e dockerfile e compose

--- src/LGA.Clima.Api/Controllers/PrevisaoTempoController.cs
+++ src/LGA.Clima.Api/Controllers/PrevisaoTempoController.cs
@@ -2,10 +2,16 @@
 
 namespace LGA.Clima.Api.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class PrevisaoTempoController : ControllerBase
     {
 
-
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Api OK!");
+        }
 
 
     }
